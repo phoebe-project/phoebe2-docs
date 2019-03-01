3eff7dd6b908adf5c304f8916d358bea2d8332ce
Travis CI User <travis@example.org>
2019-03-01 20:37:45 +0000

    TRAVIS: updating ipynb->py sources

--- examples/py/animation_binary_complete.py
+++ examples/py/animation_binary_complete.py
@@ -4,7 +4,7 @@
 # Complete Binary Animation
 # ============================
 # 
-# **NOTE**: animating within Jupyter notebooks can be very resource intensive.  This script will likely run quicker as a Python script.
+# **NOTE**: animating within Jupyter notebooks can be very resource intensive.  This script will likely run much quicker as a Python script.
 # 
 # Setup
 # -----------------------------
@@ -38,7 +38,7 @@ b = phoebe.default_binary()
 # In[2]:
 
 
-times = np.linspace(0,1,51)
+times = np.linspace(0,1,21)
 
 
 # In[3]:
@@ -73,7 +73,7 @@ b.run_compute(irrad_method='none')
 # 
 # See the [Animations Tutorial](../tutorials/animations) for more examples and details.
 # 
-# Here we'll create a figure with multiple subplots.  The top row will be the light curve and RV curve.  The bottom three rows will be various representations of the mesh (intensities, rvs, and visibilities).
+# Here we'll create a figure with multiple subplots.  The top row will be the light curve and RV curve.  The bottom three subplots will be various representations of the mesh (intensities, rvs, and visibilities).
 # 
 # We'll do this by making separate calls to plot, passing the matplotlib subplot location for each axes we want to create.  We can then call `b.show(animate=True)` or `b.save('anim.gif', animate=True)`.
 
@@ -86,7 +86,8 @@ b['mesh@model'].plot(fc='intensities@lc01', ec='None', axpos=425)
 b['mesh@model'].plot(fc='rvs@rv01', ec='None', axpos=427)
 b['mesh@model'].plot(fc='visibilities', ec='None', y='ws', axpos=224)
 
-b.savefig('animation_binary_complete.gif', tight_layouot=True, draw_sidebars=False, animate=True, save_kwargs={'writer': 'imagemagick'})
+fig = plt.figure(figsize=(11,4))
+b.savefig('animation_binary_complete.gif', fig=fig, tight_layouot=True, draw_sidebars=False, animate=True, save_kwargs={'writer': 'imagemagick'})
 
 
 # ![animation](animation_binary_complete.gif)
