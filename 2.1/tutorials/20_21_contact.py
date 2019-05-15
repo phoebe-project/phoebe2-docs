b15480212d50ff8bdea95880a6a366ac538858fb
Travis CI User <travis@example.org>
2019-05-15 18:51:35 +0000

    TRAVIS: updating ipynb->py sources

---
+++ 2.1/tutorials/20_21_contact.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 2.0 - 2.1 Migration: Contact Systems
+# ============================
+
+# Let's first make sure we have the latest version of PHOEBE 2.1 installed. (You can comment out this line if you don't use pip for your installation or don't want to update to the latest release).
+
+# In[ ]:
+
+
+get_ipython().system('pip install -I "phoebe>=2.1,<2.2"')
+
+
+# In PHOEBE 2.1, [rpole and potential have been replaced with requiv](20_21_requiv).  That means that the constraint used to handle semidetached systems has also changed.
+
+# In[1]:
+
+
+import phoebe
+b = phoebe.default_binary(contact_binary=True)
+
+
+# ### requiv_max and requiv_min
+
+# In PHOEBE 2.1, there is a new constrained Parameter for contact systems which tell the maximum value allowed for requiv before overflow occurs.
+
+# In[4]:
+
+
+print b['requiv_max@constraint']
+
+
+# Similarly, there is another constrained Parameter that defines the minimum allowed value for requiv before the system would no longer be in contact.
+
+# In[3]:
+
+
+print b['requiv_min@constraint']
+
+
+# ### requiv, pot, and fillout_factor
+
+# Note that requiv and requiv_min exist for both the primary and secondary components.  If we look at requiv, we'll see that that is also defined for both components, with another constraint dictating that the secondary requiv must correspond to the same potential of the contact envelope.
+
+# In[5]:
+
+
+print b['requiv']
+
+
+# The value of that potential as well as the corresponding fillout_factor is also exposed (and attached to the envelope object).  In PHOEBE 2.0, pot was the input Parameter for contacts, but is now constrained with requiv of the primary star the free parameter, by default.
+
+# In[8]:
+
+
+print b['pot']
+
+
+# In[9]:
+
+
+print b['fillout_factor']
+
+
+# If desired, you can flip these constraints to allow any one of the three to be free (and the other two constrained).  For more information see the [critical radii: contact systems tutorial](./requiv_crit_contact.ipynb).
---
+++ 2.1/tutorials/mpi.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Advanced: Running PHOEBE in MPI
+# ============================
+# 
+# Setup
+# -----------------------------
+
+# Let's first make sure we have the latest version of PHOEBE 2.1 installed. (You can comment out this line if you don't use pip for your installation or don't want to update to the latest release).
+
+# In[ ]:
+
+
+get_ipython().system('pip install -I "phoebe>=2.1,<2.2"')
+
+
+# In[1]:
+
+
+import phoebe
+
+
+# MPI Modes
+# ------------
+
+# As of the 2.1 release, PHOEBE officially support parallelization using MPI.  Note that the 2.1.1 release fixed a bug related to using PHOEBE within non-openmpi environments.
+
+# There are several "modes of operation" depending on your settings and whether you're running your script within python or mpirun.  You can enable/disable MPI within phoebe by placing [phoebe.mpi_on()](../api/phoebe.mpi_on.md) or [phoebe.mpi_off()](../api/phoebe.mpi_off.md) at the top of your script.  If you do not do this, MPI will be enabled by default if within mpirun and disabled otherwise.
+# 
+# When MPI is enabled, PHOEBE will do the following:
+# * if within mpirun: uses PHOEBE's built-in per-dataset or per-time parallelization.  The main code you write in your script is executed on a single processor, but during [run_compute](../api/phoebe.frontend.bundle.Bundle.run_compute.md) the task is divided among the available resources.
+# * if not within mpirun (ie. in a serial python environment): will spawn a separate thread at phoebe.frontend.bundle.Bundle.run_compute, using number of processors sent to [phoebe.mpi_on](../api/phoebe.mpi_on.md) (for example: phoebe.mpi_on(nprocs=4)). 
+# 
+# When MPI is disabled, PHOEBE will do the following:
+# * if within mpirun: PHOEBE will run equally on all processors. The user can customize parallelization with access to phoebe.mpi.nprocs, phoebe.mpi.myrank.  Your script runs equally on each processor, meaning you have multiple (separate) copies of the bundle.
+# * if not within mpirun (ie. in a serial python environment): PHOEBE will run on a single processor in serial-mode. 
+
+# Accessing/Changing MPI Settings
+# --------------------
+
+# To check the currently adopted settings, as well as quickly access information needed for manually doing your own parallelization, access the phoebe.mpi object.
+
+# In[3]:
+
+
+print(phoebe.mpi.enabled)
+
+
+# In[4]:
+
+
+print(phoebe.mpi.mode)
+
+
+# In[5]:
+
+
+phoebe.mpi_on()
+
+
+# In[6]:
+
+
+print(phoebe.mpi.enabled)
+
+
+# In[7]:
+
+
+print(phoebe.mpi.mode)
+
+
+# In[8]:
+
+
+print(phoebe.mpi.myrank)
+
+
+# In[9]:
+
+
+print(phoebe.mpi.nprocs)
+
+
+# PHOEBE determines whether the current script is running within an MPI environment by checking for environment variables set by mpirun/mpiexec.  If you run into any issues with PHOEBE not behaving as expected, check to see whether PHOEBE thinks its within mpirun.
+
+# In[10]:
+
+
+print(phoebe.mpi.within_mpirun)
+
+
+# If this gives False when you're calling PHOEBE within mpirun, please contact us and we'll do our best to add support for your MPI setup.
