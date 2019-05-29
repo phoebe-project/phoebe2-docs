76d05f39e45dab8ac3f84ba29b3e2df3bb53a3fa
Travis CI User <travis@example.org>
2019-05-29 13:29:18 +0000

    TRAVIS: updating ipynb->py sources

--- 2.0/tutorials/pblum.py
+++ 2.0/tutorials/pblum.py
@@ -76,7 +76,7 @@ print b['pblum_ref']
 print b['pblum_ref@primary']
 
 
-# The 'pblum' parameter is only relevant for each component-dataset pair in which pbscale==pblum.  This component will then have its intensities scaled such that they match the value provided by pblum.  In general, a pblum of 4pi will result in an out-of-eclipse flux of ~1.
+# The 'pblum' parameter is only relevant for each component-dataset pair in which pblum_ref=='self'.  This component will then have its intensities scaled such that they match the value provided by pblum.  In general, a pblum of 4pi will result in an out-of-eclipse flux of ~1.
 
 # In[7]:
 
