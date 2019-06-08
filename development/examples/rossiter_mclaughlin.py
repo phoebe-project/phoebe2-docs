b692ed5ace5e50ef458c5a9c358359af1188d357
Travis CI User <travis@example.org>
2019-06-08 23:54:51 +0000

    TRAVIS: updating ipynb->py sources

--- development/examples/rossiter_mclaughlin.py
+++ development/examples/rossiter_mclaughlin.py
@@ -32,7 +32,7 @@ import numpy as np
 b = phoebe.default_binary()
 
 
-# Let's make a significant mass ratio and radius ratio and spin up the primary star.
+# Now we'll try to exaggerate the effect by spinning up the secondary component.
 
 # In[2]:
 
@@ -47,14 +47,14 @@ b.set_value('syncpar', component='secondary', value=1.3)
 # Adding Datasets
 # -------------------
 # 
-# Now we'll add radial velocity, line profile, and mesh datasets.  We'll compute the rvs through the whole orbit, but the mesh and line profiles just at the times that we want to plot for an animation (which we'll do by concatenating three arrays so the animation "slow down" during eclipse).
+# We'll add radial velocity, line profile, and mesh datasets.  We'll compute the rvs through the whole orbit, but the mesh and line profiles just at the times that we want to plot for an animation (which we'll do by concatenating three arrays with different stepsizes so the animation "slows down" during the eclipse).
 
 # In[3]:
 
 
-anim_times= np.concatenate((np.linspace(0.4, 0.48, 21), 
-                            np.linspace(0.48, 0.52, 21), 
-                            np.linspace(0.52, 0.6, 21)))
+anim_times= np.concatenate((np.arange(0.4, 0.475, 0.005), 
+                            np.arange(0.475, 0.525, 0.001), 
+                            np.arange(0.525, 0.6, 0.005)))
 
 
 # We'll add two identical datasets, one where we compute only dynamical RVs (won't include Rossiter-McLaughlin) and another where we compute flux-weighted RVs (will include Rossiter-McLaughlin).
@@ -124,39 +124,31 @@ b.run_compute(irrad_method='none')
 
 afig, mplfig = b.plot(kind='rv',
                       c={'primary': 'b', 'secondary': 'r'}, 
-                      ls={'numericalrvs': 'dashed', 'dynamicalrvs': 'solid'},
+                      ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
                       show=True)
-
-
-# We'll remove the dynamical RVs entirely since we don't want to include them from here on out.
-
-# In[10]:
-
-
-#b.remove_dataset('dynamicalrvs')
 
 
 # Now let's make a plot of the line profiles and mesh during ingress to visualize what's happening.  
 
-# In[13]:
+# In[10]:
 
 
 afig, mplfig= b.plot(time=0.48,
                      fc='rvs@numericalrvs', ec='none',
                      show=True,
                      c={'primary': 'blue', 'secondary': 'red'},
-                     ls={'numericalrvs': 'dashed', 'dynamicalrvs': 'solid'},
+                     ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
                      highlight={'numericalrvs': True, 'dynamicalrvs': False},
                      tight_layout=True,
                      axpos={'mesh': 211, 'rv': 223, 'lp': 224},
                      xlim={'rv': (0.4, 0.6)}, ylim={'rv': (-100, 100)})
 
 
-# Here we can see that the blue part of the back star is eclipsed, distorting the line profile, and causing the apparent center of the line profile to be shifted to the right/red.
+# Here we can see that star in front (smaller radius, blue in the bottom panels) is eclipsing more of the blue-shifted part of the back star (larger radius, red in the bottom panels), distorting the line profile, causing the apparent center of the line profile to be shifted to the right/red, and therefore the radial velocities to be articially increased as compared to the dynamical RVs.
 
 # Now let's animate the same figure in time.
 
-# In[14]:
+# In[11]:
 
 
 afig, mplanim = b.plot(times=anim_times,
@@ -164,7 +156,7 @@ afig, mplanim = b.plot(times=anim_times,
                        animate=True, save='rossiter_mclaughlin.gif',
                        save_kwargs={'writer': 'imagemagick'},
                        c={'primary': 'blue', 'secondary': 'red'},
-                       ls={'numericalrvs': 'dashed', 'dynamicalrvs': 'solid'},
+                       ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
                        highlight={'numericalrvs': True, 'dynamicalrvs': False},
                        tight_layout=True, pad_aspect=False,
                        axpos={'mesh': 211, 'rv': 223, 'lp': 224},
@@ -172,9 +164,3 @@ afig, mplanim = b.plot(times=anim_times,
 
 
 # ![rm animation](rossiter_mclaughlin.gif)
-
-# In[ ]:
-
-
-
-
