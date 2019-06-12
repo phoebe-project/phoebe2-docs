33028b1c27d7e072493895e3f05cf677bca19102
Travis CI User <travis@example.org>
2019-06-12 19:45:14 +0000

    TRAVIS: updating ipynb->py sources

--- development/examples/rossiter_mclaughlin.py
+++ development/examples/rossiter_mclaughlin.py
@@ -39,22 +39,20 @@ b = phoebe.default_binary()
 
 b.set_value('q', value=0.7)
 b.set_value('incl', component='binary', value=87)
-b.set_value('requiv', component='primary', value=0.75)
+b.set_value('requiv', component='primary', value=0.8)
 b.set_value('teff', component='secondary', value=6500)
-b.set_value('syncpar', component='secondary', value=1.3)
+b.set_value('syncpar', component='secondary', value=1.5)
 
 
 # Adding Datasets
 # -------------------
 # 
-# We'll add radial velocity, line profile, and mesh datasets.  We'll compute the rvs through the whole orbit, but the mesh and line profiles just at the times that we want to plot for an animation (which we'll do by concatenating three arrays with different stepsizes so the animation "slows down" during the eclipse).
+# We'll add radial velocity, line profile, and mesh datasets.  We'll compute the rvs through the whole orbit, but the mesh and line profiles right around the eclipse - just at the times that we want to plot for an animation.
 
 # In[3]:
 
 
-anim_times= np.concatenate((np.arange(0.4, 0.475, 0.005), 
-                            np.arange(0.475, 0.525, 0.001), 
-                            np.arange(0.525, 0.6, 0.005)))
+anim_times = phoebe.arange(0.44, 0.56, 0.002)
 
 
 # We'll add two identical datasets, one where we compute only dynamical RVs (won't include Rossiter-McLaughlin) and another where we compute flux-weighted RVs (will include Rossiter-McLaughlin).
@@ -63,7 +61,7 @@ anim_times= np.concatenate((np.arange(0.4, 0.475, 0.005),
 
 
 b.add_dataset('rv', 
-              times=phoebe.linspace(0,1,101), 
+              times=phoebe.linspace(0,1,201), 
               dataset='dynamicalrvs')
 
 b.set_value_all('rv_method', dataset='dynamicalrvs', value='dynamical')
@@ -73,7 +71,7 @@ b.set_value_all('rv_method', dataset='dynamicalrvs', value='dynamical')
 
 
 b.add_dataset('rv', 
-              times=phoebe.linspace(0,1,101), 
+              times=phoebe.linspace(0,1,201), 
               dataset='numericalrvs')
 
 b.set_value_all('rv_method', dataset='numericalrvs', value='flux-weighted')
@@ -115,52 +113,78 @@ b.run_compute(irrad_method='none')
 # Plotting 
 # ---------------
 # 
-# First let's compare between the dynamical and numerical RVs.
-# 
-# The dynamical RVs show the velocity of the center of each star along the line of sight.  But the numerical method integrates over the visible surface elements, giving us what we'd observe if deriving RVs from observed spectra of the binary.  Here we do see the Rossiter McLaughlin effect.  You'll also notice that RVs are not available for the secondary star when its completely occulted (they're nans in the array).
+# Throughout all of these plots, we'll color the components green and magenta (to differentiate them from the red and blue of the RV mapping).
 
 # In[9]:
 
 
-afig, mplfig = b.plot(kind='rv',
-                      c={'primary': 'b', 'secondary': 'r'}, 
-                      ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
-                      show=True)
+colors = {'primary': 'green', 'secondary': 'magenta'}
 
 
-# Now let's make a plot of the line profiles and mesh during ingress to visualize what's happening.  
+# First let's compare between the dynamical and numerical RVs.  
+# 
+# The dynamical RVs show the velocity of the center of each star along the line of sight.  But the numerical method integrates over the visible surface elements, giving us what we'd observe if deriving RVs from observed spectra of the binary.  Here we do see the Rossiter McLaughlin effect.  You'll also notice that RVs are not available for the secondary star when its completely occulted (they're nans in the array).
 
 # In[10]:
 
 
-afig, mplfig= b.plot(time=0.48,
-                     fc='rvs@numericalrvs', ec='none',
-                     show=True,
-                     c={'primary': 'blue', 'secondary': 'red'},
-                     ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
-                     highlight={'numericalrvs': True, 'dynamicalrvs': False},
-                     tight_layout=True,
-                     axpos={'mesh': 211, 'rv': 223, 'lp': 224},
-                     xlim={'rv': (0.4, 0.6)}, ylim={'rv': (-100, 100)})
+afig, mplfig = b.plot(kind='rv',
+                      c=colors, 
+                      ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
+                      show=True)
 
 
-# Here we can see that star in front (smaller radius, blue in the bottom panels) is eclipsing more of the blue-shifted part of the back star (larger radius, red in the bottom panels), distorting the line profile, causing the apparent center of the line profile to be shifted to the right/red, and therefore the radial velocities to be articially increased as compared to the dynamical RVs.
-
-# Now let's animate the same figure in time.
+# Now let's make a plot of the line profiles and mesh during ingress to visualize what's happening. 
+# 
+# Let's go through these options:
+# * `time`: make the plot at this single time
+# * `fc`: (will be ignored by everything but the mesh): set the facecolor to the rvs column.  This will automatically apply a red-blue color mapping.
+# * `ec`: disable drawing the edges of the triangles in a separate color.  We could also set this to 'none', but then we'd be able to "see-through" the triangle edges.
+# * `c`: set the colors as defined in our dictionary above.  This will apply to the rv, lp, and horizon datasets, but will be ignored by the mesh.
+# * `ls`: set the linestyle to differentiate between numerical and dynamical rvs.
+# * `highlight`: highlight the current time on the numerical rvs only.
+# * `axpos`: define the layout of the axes so the mesh plot takes up the horizontal space it needs.
+# * `xlim`: "zoom-in" on the RM effect in the RVs, allow the others to fallback on automatic limits.
+# * `tight_layout`: use matplotlib's tight layout to ensure we have enough padding between axes to see the labels.
 
 # In[11]:
 
 
+afig, mplfig= b.plot(time=0.46,
+                     fc='rvs@numericalrvs', ec='face',
+                     c=colors,
+                     ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
+                     highlight={'numericalrvs': True, 'dynamicalrvs': False},
+                     axpos={'mesh': 211, 'rv': 223, 'lp': 224},
+                     xlim={'rv': (0.4, 0.6)}, ylim={'rv': (-80, 80)},
+                     tight_layout=True, 
+                     show=True)
+
+
+# Here we can see that star in front (green) is eclipsing more of the blue-shifted part of the back star (magenta), distorting the line profile, causing the apparent center of the line profile to be shifted to the right/red, and therefore the radial velocities to be articially increased as compared to the dynamical RVs.
+
+# Now let's animate the same figure in time.  We'll use the same arguments as the static plot above, with the following exceptions:
+# 
+# * `times`: pass our array of times that we want the animation to loop over.
+# * `pad_aspect`: pad_aspect doesn't work with animations, so we'll disable to avoid the warning messages.
+# * `animate`: self-explanatory.
+# * `save`: we could use `show=True`, but that doesn't always play nice with jupyter notebooks
+# * `save_kwargs`: may need to change these for your setup, to create a gif, passing {'writer': 'imagemagick'} is often useful.
+
+# In[12]:
+
+
 afig, mplanim = b.plot(times=anim_times,
-                       fc='rvs@numericalrvs', ec='none',
-                       animate=True, save='rossiter_mclaughlin.gif',
-                       save_kwargs={'writer': 'imagemagick'},
-                       c={'primary': 'blue', 'secondary': 'red'},
+                       fc='rvs@numericalrvs', ec='face',
+                       c=colors,
                        ls={'numericalrvs': 'solid', 'dynamicalrvs': 'dotted'},
                        highlight={'numericalrvs': True, 'dynamicalrvs': False},
                        tight_layout=True, pad_aspect=False,
                        axpos={'mesh': 211, 'rv': 223, 'lp': 224},
-                       xlim={'rv': (0.4, 0.6)}, ylim={'rv': (-100, 100)})
+                       xlim={'rv': (0.4, 0.6)}, ylim={'rv': (-80, 80)},
+                       animate=True, 
+                       save='rossiter_mclaughlin.gif',
+                       save_kwargs={'writer': 'imagemagick'})
 
 
 # ![rm animation](rossiter_mclaughlin.gif)
---
+++ development/examples/spot_transit.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Spot Transit
+# ============================
+# 
+# Setup
+# -----------------------------
+
+# Let's first make sure we have the latest version of PHOEBE 2.2 installed. (You can comment out this line if you don't use pip for your installation or don't want to update to the latest release).
+
+# In[ ]:
+
+
+get_ipython().system('pip install -I "phoebe>=2.2,<2.3"')
+
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# As always, let's do imports and initialize a logger and a new bundle.  See [Building a System](../tutorials/building_a_system.ipynb) for more details.
+
+# In[1]:
+
+
+import phoebe
+import numpy as np
+
+b = phoebe.default_binary()
+
+
+# Let's set reasonable (although not necessarily physical) values for the secondary component.
+
+# In[2]:
+
+
+b.flip_constraint('mass@secondary', solve_for='q')
+b.set_value(qualifier='mass', component='secondary', value=0.2)
+b.set_value(qualifier='requiv', component='secondary', value=0.2)
+b.set_value(qualifier='teff', component='secondary', value=100)
+
+
+# We'll add a spot to the primary component.
+
+# In[3]:
+
+
+b.add_spot(component='primary', 
+           relteff=0.90, 
+           long=0, 
+           colat=90, 
+           radius=20, 
+           feature='spot01')
+
+
+# Adding Datasets
+# -------------------
+
+# In[4]:
+
+
+b.add_dataset('lc', compute_times=phoebe.linspace(-0.1, 0.1, 201))
+
+
+# Because we have such a cool transiting object, we'll have to use blackbody atmospheres and manually provide limb-darkening.
+
+# In[5]:
+
+
+b.set_value(qualifier='atm', component='secondary', value='blackbody')
+b.set_value(qualifier='ld_mode', component='secondary', value='func_provided')
+
+
+# In[6]:
+
+
+anim_times = phoebe.linspace(-0.1, 0.1, 101)
+
+
+# In[7]:
+
+
+b.add_dataset('mesh', compute_times=anim_times, coordinates='uvw', columns='teffs')
+
+
+# Running Compute
+# --------------------
+
+# In[8]:
+
+
+b.run_compute(distortion_method='sphere', irrad_method='none')
+
+
+# Plotting 
+# ---------------
+
+# In[9]:
+
+
+print(np.min(b.get_value('teffs', time=0.0, component='primary')), np.max(b.get_value('teffs', time=0.0, component='primary')))
+
+
+# Let's go through these options:
+# * `time`: make the plot at this single time
+# * `fc`: (will be ignored by everything but the mesh): set the facecolor to the teffs column.
+# * `fcmap`: use 'plasma' colormap instead of the default to avoid whites.
+# * `fclim`: set the limits on facecolor so that the much cooler transiting object doesn't drive the entire range.
+# * `ec`: disable drawing the edges of the triangles in a separate color.  We could also set this to 'none', but then we'd be able to "see-through" the triangle edges.
+# * `tight_layout`: use matplotlib's tight layout to ensure we have enough padding between axes to see the labels.
+# 
+
+# In[10]:
+
+
+afig, mplfig = b.plot(time=0.0,
+                      fc='teffs', fcmap='plasma', fclim=(5000, 6000), 
+                      ec='face', 
+                      tight_layout=True,
+                      show=True)
+
+
+# Now let's animate the same figure in time.  We'll use the same arguments as the static plot above, with the following exceptions:
+# 
+# * `times`: pass our array of times that we want the animation to loop over.
+# * `consider_for_limits`: for the mesh panel, keep the primary star centered and allow the transiting object to move in and out of the frame.
+# * `pad_aspect`: pad_aspect doesn't work with animations, so we'll disable to avoid the warning messages.
+# * `animate`: self-explanatory.
+# * `save`: we could use `show=True`, but that doesn't always play nice with jupyter notebooks
+# * `save_kwargs`: may need to change these for your setup, to create a gif, passing {'writer': 'imagemagick'} is often useful.
+
+# In[11]:
+
+
+afig, mplfig = b.plot(times=anim_times,
+                      fc='teffs', fcmap='plasma', fclim=(5000, 6000), 
+                      ec='face', 
+                      consider_for_limits={'primary': True, 'secondary': False},
+                      tight_layout=True, pad_aspect=False,
+                      animate=True, 
+                      save='spot_transit.gif',
+                      save_kwargs={'writer': 'imagemagick'})
+
+
+# ![gif](spot_transit.gif)
