7063fed731f88190dee0d2636d0e48ec4de8c120
Travis CI User <travis@example.org>
2019-06-23 14:38:27 +0000

    TRAVIS: updating ipynb->py sources

--- 2.1/examples/binary_misaligned_spots.py
+++ 2.1/examples/binary_misaligned_spots.py
@@ -36,16 +36,22 @@ logger = phoebe.logger()
 b = phoebe.default_binary()
 
 
+# In[3]:
+
+
+b.set_value(qualifier='pitch', component='primary', value=30)
+
+
 # Model without Spots
 # --------------------------
 
-# In[3]:
+# In[4]:
 
 
 b.add_dataset('lc', times=phoebe.linspace(0,1,101))
 
 
-# In[4]:
+# In[5]:
 
 
 b.run_compute(irrad_method='none', model='no_spot')
@@ -54,33 +60,48 @@ b.run_compute(irrad_method='none', model='no_spot')
 # Adding Spots
 # ---------------------
 
-# Let's add a spot to the primary component in our binary.
+# Let's add a spot to the primary component in our binary, which we have already misaligned by 30 degrees in pitch.
 # 
 # The 'colat' parameter defines the colatitude on the star measured from its North (spin) Pole.  The 'long' parameter measures the longitude of the spot - with longitude = 0 being defined as pointing towards the other star at t0.  See the [spots tutorial](../tutorials/spots.ipynb) for more details.
-
-# In[5]:
-
-
-b.add_feature('spot', component='primary', feature='spot01', relteff=0.9, radius=30, colat=45, long=90)
-
+# 
+# We'll place this spot at the South Pole, which should be pointing towards the observer because we pitched the north pole away from the observer.
 
 # In[6]:
+
+
+b.add_feature('spot', component='primary', feature='spot01', relteff=0.9, radius=15, colat=180, long=0)
+
+
+# We'll also add a mesh dataset so that we can see the positioning of the spot with respect to the misaligned component.
+
+# In[7]:
+
+
+b.add_dataset('mesh', times=[0.75], columns=['teffs'])
+
+
+# In[8]:
 
 
 b.run_compute(irrad_method='none', model='with_spot')
 
 
+# Location of Spot
+# ------------------------------
+
+# In[11]:
+
+
+afig, mplfig = b.plot(kind='mesh', fc='teffs', fcmap='plasma', ec='none', show=True)
+
+
 # Comparing Light Curves
 # ------------------------------
 
-# In[7]:
+# Note that the pitch means the polar spot is always facing towards the observer slightly, and so is always visible (unless eclipsed).
+
+# In[12]:
 
 
-afig, mplfig = b.plot(show=True, legend=True)
-
-
-# In[ ]:
-
-
-
+afig, mplfig = b.plot(kind='lc', show=True, legend=True)
 
