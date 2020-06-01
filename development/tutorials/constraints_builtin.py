c9089a6755c9f051014747473a4cfa146d919a2d
Travis CI User <travis@example.org>
2020-06-01 20:00:22 +0000

    TRAVIS: updating ipynb->py sources

--- development/tutorials/constraints_builtin.py
+++ development/tutorials/constraints_builtin.py
@@ -9,7 +9,7 @@
 
 # Let's first make sure we have the latest version of PHOEBE 2.2 installed. (You can comment out this line if you don't use pip for your installation or don't want to update to the latest release).
 
-# In[ ]:
+# In[1]:
 
 
 get_ipython().system('pip install -I "phoebe>=2.2,<2.3"')
@@ -17,7 +17,7 @@ get_ipython().system('pip install -I "phoebe>=2.2,<2.3"')
 
 # As always, let's do imports and initialize a logger and a new Bundle.  See [Building a System](building_a_system.ipynb) for more details.
 
-# In[1]:
+# In[2]:
 
 
 import phoebe
@@ -33,13 +33,13 @@ b = phoebe.default_binary()
 # Built-in Constraints
 # -----------------------------
 # 
-# There are a number of [built-in constraints](../api/phoebe.parameters.constraint.md) that can be applied to our system.  Those added by default are all listed below:
+# There are a number of [built-in constraints](../api/phoebe.parameters.constraint.md) that can be applied to our system.  Those added by default are listed below as well as in the API docs for [b.add_constraint](../api/phoebe.frontend.bundle.Bundle.add_constraint.md):
 
 # ### asini
 # 
 # This constraint handles computing the projected semi-major axis along the line of sight and can be automatically inverted to solve for either 'asini', 'sma', or 'incl'.
 
-# In[6]:
+# In[3]:
 
 
 b['asini@constraint']
@@ -51,13 +51,13 @@ b['asini@constraint']
 # 
 # Both can be inverted to also automatically solve for 'ecc' or 'per0'.
 
-# In[7]:
+# In[4]:
 
 
 b['esinw@constraint']
 
 
-# In[8]:
+# In[5]:
 
 
 b['ecosw@constraint']
@@ -69,7 +69,7 @@ b['ecosw@constraint']
 # 
 # Currently, this constraint only supports inverting to be solved for 't0_supconj' (ie you cannot *automatically* invert this constraint to constraint phshift or per0).
 
-# In[9]:
+# In[6]:
 
 
 b['t0_perpass@constraint']
@@ -79,19 +79,19 @@ b['t0_perpass@constraint']
 # 
 # This constraint handles the simple conversion to frequency from period - whether that be rotational or orbital - and does support inversion to solve for 'period'.
 
-# In[10]:
+# In[7]:
 
 
 b['freq@constraint']
 
 
-# In[11]:
+# In[8]:
 
 
 b['freq@binary@constraint']
 
 
-# In[12]:
+# In[9]:
 
 
 b['freq@primary@constraint']
@@ -103,13 +103,13 @@ b['freq@primary@constraint']
 # 
 # It can be inverted to solve for 'sma' or 'period' (in addition to 'mass'), but **not** 'q'.
 
-# In[13]:
+# In[10]:
 
 
 b['mass@constraint']
 
 
-# In[14]:
+# In[11]:
 
 
 b['mass@primary@constraint']
@@ -121,29 +121,47 @@ b['mass@primary@constraint']
 # 
 # This currently can be inverted to solve for 'sma' of the parent orbit, but **not** 'q'.
 
-# In[15]:
+# In[12]:
 
 
 b['sma@constraint']
 
 
-# In[16]:
+# In[13]:
 
 
 b['sma@primary@constraint']
+
+
+# ### component asini
+# 
+# This constraint handles computing the projected semi-major axis of a component about the **center of mass** of its parent orbit.  Note that this is **not** the same as the asini **of** the parent orbit.
+# 
+# This currently can be inverted to solve for 'sma' of the parent orbit, but **not** 'q' or 'incl'.
+
+# In[14]:
+
+
+b['asini@component']
+
+
+# In[15]:
+
+
+b['asini@primary@constraint']
 
 
 # ### requiv_max
 # 
 # This constraint handles solving for the maxium equivalent radius (for a detached system).
 
-# In[17]:
+# In[16]:
 
 
 b['requiv_max@constraint']
 
 
-# In[18]:
+# In[17]:
 
 
 b['requiv_max@primary@constraint']
@@ -155,13 +173,13 @@ b['requiv_max@primary@constraint']
 # 
 # It can be inverted to solve for any of the three parameters 'period' (both rotational and orbital) and 'syncpar'.
 
-# In[19]:
+# In[18]:
 
 
 b['period@constraint']
 
 
-# In[20]:
+# In[19]:
 
 
 b['period@primary@constraint']
@@ -171,25 +189,25 @@ b['period@primary@constraint']
 # 
 # pitch constrains the relation between the orbital and rotational inclination whereas yaw constrains the relation between the orbital and rotational long_an.  When pitch **and** yaw are set to 0, the system is aligned.
 
-# In[21]:
+# In[20]:
 
 
 b['incl@constraint']
 
 
-# In[22]:
+# In[21]:
 
 
 b['incl@primary@constraint']
 
 
-# In[23]:
+# In[22]:
 
 
 b['long_an@constraint']
 
 
-# In[24]:
+# In[23]:
 
 
 b['long_an@primary@constraint']
