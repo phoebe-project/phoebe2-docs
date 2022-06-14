d7dd8726cfaf5e28a36c7fa060d7f935f2b853c7
gh-actions <gh-actions>
2022-06-14 01:18:56 +0000

    building development docs
    automatically generated by github build-docs action

--- development/tutorials/passbands.py
+++ development/tutorials/passbands.py
@@ -3,20 +3,19 @@
 
 # # Adding new passbands to PHOEBE
 # 
-# In this tutorial we will show you how to add your own passband to PHOEBE. Adding a passband involves:
+# In this tutorial we will show you how to add your own passband to PHOEBE. Adding a custom passband involves:
 # 
+# * downloading and setting up model atmosphere tables;
 # * providing a passband transmission function;
-# * defining and registering parameters of the passband;
+# * defining and registering passband parameters;
 # * computing blackbody response for the passband;
-# * \[optional\] computing Castelli & Kurucz (2004) response for the passband;
-# * \[optional\] computing Castelli & Kurucz (2004) limb darkening coefficients;
-# * \[optional\] computing Castelli & Kurucz (2004) limb darkening integrals;
+# * \[optional\] computing Castelli & Kurucz (2004) passband tables;
+# * \[optional\] computing Husser et al. (2013) PHOENIX passband tables;
 # * \[optional\] if the passband is one of the passbands included in the Wilson-Devinney code, importing the WD response; and
 # * saving the generated passband file.
-# 
-# 
+# <!-- * \[optional\] computing Werner et al. (2012) TMAP passband tables; -->
 
-# Let's first make sure we have the latest version of PHOEBE 2.4 installed (uncomment this line if running in an online notebook session such as colab).
+# Let's first make sure we have the correct version of PHOEBE installed. Uncomment the following line if running in an online notebook session such as colab.
 
 # In[ ]:
 
@@ -24,10 +23,34 @@
 #!pip install -I "phoebe>=2.4,<2.5"
 
 
-# I don't care about the details, just show/remind me how it's done
-# -------------------------------------------------------------------
+# If you plan on computing model atmosphere intensities (as opposed to only blackbody intensities), you will need to download atmosphere tables and unpack them into a local directory of your choice. Keep in mind that this will take a long time. Plan to go for lunch or leave it overnight. The good news is that this needs to be done only once. For the purpose of this document, we will use a local `tables/` directory and assume that we are computing intensities for all available model atmospheres:
+# ```
+# mkdir tables
+# cd tables
+# wget http://phoebe-project.org/static/atms/ck2004.tgz
+# wget http://phoebe-project.org/static/atms/phoenix.tgz
+# ```
+# <!-- wget http://phoebe-project.org/static/atms/tmap.tgz -->
 # 
-# Makes sense, and we don't judge: you want to get to science. Provided that you have the passband transmission file available and the ck2004 database already downloaded, the sequence that will generate/register a new passband is:
+# Once the data are downloaded, unpack the archives:
+# ```
+# tar xvzf ck2004.tgz
+# tar xvzf phoenix.tgz
+# ```
+# <!-- tar xvzf tmap.tgz -->
+# 
+# That should leave you with the following directory structure:
+# ```
+# tables
+# |____ck2004
+# |     |____TxxxxxGxxPxx.fits (3800 files)
+# |____phoenix
+# |     |____ltexxxxx-x.xx-x.x.PHOENIX-ACES-AGSS-COND-SPECINT-2011.fits (7260 files)
+# ```
+
+# ## I don't care about the details, just show/remind me how it's done
+# 
+# Makes sense, and we don't judge: you want to get to science. Provided that you have the [passband transmission file](https://raw.githubusercontent.com/phoebe-project/phoebe2-docs/2.4/tutorials/my_passband.ptf) available and the atmosphere tables already downloaded, the sequence that will generate/register a new passband is:
 
 # In[ ]:
 
@@ -37,29 +60,51 @@ from phoebe import u
 
 # Register a passband:
 pb = phoebe.atmospheres.passbands.Passband(
-    ptf='my_passband.ptf', pbset='Custom', pbname='mypb',
-    effwl=330., wlunits=u.nm, calibrated=True,
-    reference='A completely made-up passband published in Nowhere (2017)', version=1.0,
-    comments='This is my first custom passband')
+    ptf='my_passband.ptf',
+    pbset='Custom',
+    pbname='mypb',
+    effwl=330,
+    wlunits=u.nm,
+    calibrated=True,
+    reference='A completely made-up passband published in Nowhere (2017)',
+    version=1.0,
+    comments='This is my first custom passband'
+)
 
 # Blackbody response:
 pb.compute_blackbody_response()
 
-# Castelli & Kurucz (2004) response:
-pb.compute_ck2004_response(path='ck2004')
-pb.compute_ck2004_intensities(path='ck2004')
+# CK2004 response:
+pb.compute_ck2004_response(path='tables/ck2004')
+pb.compute_ck2004_intensities(path='tables/ck2004')
 pb.compute_ck2004_ldcoeffs()
 pb.compute_ck2004_ldints()
+
+# PHOENIX response:
+pb.compute_phoenix_response(path='tables/phoenix')
+pb.compute_phoenix_intensities(path='tables/phoenix')
+pb.compute_phoenix_ldcoeffs()
+pb.compute_phoenix_ldints()
+
+# Impute missing values from the PHOENIX model atmospheres:
+pb.impute_atmosphere_grid(pb._phoenix_energy_grid)
+pb.impute_atmosphere_grid(pb._phoenix_photon_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ld_energy_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ld_photon_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ldint_energy_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ldint_photon_grid)
+for i in range(len(pb._phoenix_intensity_axes[3])):
+    pb.impute_atmosphere_grid(pb._phoenix_Imu_energy_grid[:,:,:,i,:])
+    pb.impute_atmosphere_grid(pb._phoenix_Imu_photon_grid[:,:,:,i,:])
 
 # Wilson-Devinney response:
 pb.import_wd_atmcof('atmcofplanck.dat', 'atmcof.dat', 22)
 
 # Save the passband:
-pb.save('my_passband.pb')
+pb.save('my_passband.fits')
 
 
-# Getting started
-# -----------------
+# ## Getting started
 # 
 # Let us start by importing phoebe, numpy and matplotlib:
 
@@ -75,8 +120,7 @@ import matplotlib.pyplot as plt
 logger = phoebe.logger(clevel='WARNING')
 
 
-# Passband transmission function
-# -----------------------------------
+# ## Passband transmission function
 # 
 # The passband transmission function is typically a user-provided two-column file. The first column is wavelength, and the second column is passband transmission. For the purposes of this tutorial, we will simulate the passband as a uniform box.
 
@@ -88,7 +132,7 @@ ptf = np.zeros(len(wl))
 ptf[(wl>=320) & (wl<=340)] = 1.0
 
 
-# Let us plot this simulated passband transmission function to see what it looks like:
+# Let us plot this mock passband transmission function to see what it looks like:
 
 # In[3]:
 
@@ -142,7 +186,7 @@ pb = phoebe.atmospheres.passbands.Passband(
 # Computing blackbody response
 # --------------------------------
 # 
-# To significantly speed up calculations, passband coefficients are stored in lookup tables instead of computing the intensities over and over again on the fly. Computed passband tables are tagged in the `content` property of the class:
+# To significantly speed up calculations, passband intensities are stored in lookup tables instead of computing them over and over again on the fly. Computed passband tables are tagged in the `content` property of the class:
 
 # In[6]:
 
@@ -166,7 +210,7 @@ pb.compute_blackbody_response()
 pb.content
 
 
-# We can now test-drive the blackbody lookup table we just created. For this we will use a low-level `Passband` class method that computes normal emergent passband intensity, `Inorm()`. For the sake of simplicity, we will turn off limb darkening by setting `ld_func` to `'linear'` and `ld_coeffs` to `'[0.0]'`:
+# We can now test-drive the blackbody lookup table we just created. For this we will use a low-level class method that computes normal emergent passband intensity, `Inorm()`. For the sake of simplicity, we will turn off limb darkening by setting `ld_func` to `'linear'` and `ld_coeffs` to `'[0.0]'`:
 
 # In[9]:
 
@@ -182,7 +226,7 @@ pb.Inorm(Teff=5772, atm='blackbody', ld_func='linear', ld_coeffs=[0.0])
 jV = phoebe.get_passband('Johnson:V')
 teffs = np.linspace(5000, 8000, 100)
 plt.xlabel('Temperature [K]')
-plt.ylabel('Inorm [W/m^2/A]')
+plt.ylabel('Inorm [W/m^3]')
 plt.plot(teffs, pb.Inorm(teffs, atm='blackbody', ld_func='linear', ld_coeffs=[0.0]), label='mypb')
 plt.plot(teffs, jV.Inorm(teffs, atm='blackbody', ld_func='linear', ld_coeffs=[0.0]), label='jV')
 plt.legend(loc='lower right')
@@ -194,27 +238,17 @@ plt.show()
 # Computing Castelli & Kurucz (2004) response
 # -------------------------------------------------
 # 
-# For any real science you will want to generate model atmosphere tables. The default choice in PHOEBE are the models computed by Fiorella Castelli and Bob Kurucz ([website](http://wwwuser.oats.inaf.it/castelli/), [paper](https://arxiv.org/abs/astro-ph/0405087)) that feature new opacity distribution functions. In principle, you can generate PHOEBE-compatible tables for *any* model atmospheres, but that would require a bit of book-keeping legwork in the PHOEBE backend. Contact [Andrej Prša](mailto:aprsa@villanova.edu) to discuss an extension to other model atmospheres.
+# For any real science you will want to generate model atmosphere tables. The default choice in PHOEBE are the models computed by Fiorella Castelli and Bob Kurucz ([website](http://wwwuser.oats.inaf.it/castelli/), [paper](https://arxiv.org/abs/astro-ph/0405087)) that feature new opacity distribution functions. In principle, you can generate PHOEBE-compatible tables for *any* model atmospheres, but that would require a bit of book-keeping legwork in the PHOEBE backend. [Contact us](mailto:aprsa@villanova.edu) to discuss an extension to other model atmospheres.
 # 
-# To compute Castelli & Kurucz (2004) tables for the passband of your choice, you will need to download a precomputed database of absolute intensities. This database is *huge*, so beware. You will need approximately 140GB of free space. Once you are sure you have this kind of space available, proceed to download the database tarball (28GB):
-# 
-# ```
-# [cd into a parent directory that will hold the database]
-# wget http://phoebe-project.org/static/ck2004.tgz
-# tar xzf ck2004.tgz
-# ```
-# 
-# Keep in mind that this will take a long time. Plan to go for lunch or leave it overnight. The good news is that this needs to be done only once.
-# 
-# Once the database is unpacked, you are ready to compute the tables. We start with the ck2004 response table:
+# To compute Castelli & Kurucz (2004) passband tables, we will use the previously downloaded model atmospheres. We start with the ck2004 normal intensities:
 
 # In[11]:
 
 
-pb.compute_ck2004_response(path='ck2004', verbose=False)
+pb.compute_ck2004_response(path='tables/ck2004', verbose=False)
 
 
-# Note, of course, that you will need to change the `path` to point to the directory where you unpacked the ck2004 database. The verbosity parameter `verbose` will report on the progress as computation is being done. Depending on your computer speed, this step will take ~10 minutes to complete. We can now check the passband's `content` attribute again:
+# Note, of course, that you will need to change the `path` to point to the directory where you unpacked the ck2004 tables. The verbosity parameter `verbose` will report on the progress as computation is being done. Depending on your computer speed, this step will take up to a minute to complete. We can now check the passband's `content` attribute again:
 
 # In[12]:
 
@@ -230,7 +264,7 @@ pb.content
 loggs = np.ones(len(teffs))*4.43
 abuns = np.zeros(len(teffs))
 plt.xlabel('Temperature [K]')
-plt.ylabel('Inorm [W/m^2/A]')
+plt.ylabel('Inorm [W/m^3]')
 plt.plot(teffs, pb.Inorm(teffs,               atm='blackbody', ld_func='linear', ld_coeffs=[0.0]), label='blackbody')
 plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='ck2004',    ld_func='linear', ld_coeffs=[0.0]), label='ck2004')
 plt.legend(loc='lower right')
@@ -242,10 +276,10 @@ plt.show()
 # In[14]:
 
 
-pb.compute_ck2004_intensities(path='ck2004', verbose=False)
+pb.compute_ck2004_intensities(path='tables/ck2004', verbose=False)
 
 
-# This step will allow PHOEBE to compute all direction-dependent intensities on the fly, including the interpolation of the limb darkening coefficients that is model-independent. When limb darkening models are preferred (for example, when you don't quite trust direction-dependent intensities from the model atmosphere), we need to calculate two more tables: one for limb darkening coefficients and the other for the integrated limb darkening. That is done by two methods that do not take appreciable time to complete:
+# This step will allow PHOEBE to compute all direction-dependent intensities on the fly, including the interpolation of the limb darkening coefficients that is model-independent. When limb darkening models are preferred (for example, when you don't quite trust direction-dependent intensities from the model atmosphere), we need to calculate two more tables: one for limb darkening coefficients and the other for the integrated limb darkening. That is done by two methods that can take a couple of minutes to complete:
 
 # In[15]:
 
@@ -256,14 +290,59 @@ pb.compute_ck2004_ldints()
 
 # This completes the computation of Castelli & Kurucz auxiliary tables.
 # 
-# Importing Wilson-Devinney response
-# ---------------------------------------
+# ## Computing PHOENIX response
+# 
+# PHOENIX is a 3-D model atmosphere code. Because of that, it is more complex and better behaved for cooler stars (down to ~2300K). The steps to compute PHOENIX intensity tables are analogous to the ones we used for ck2004; so we can do all of them in a single step:
+
+# In[16]:
+
+
+pb.compute_phoenix_response(path='tables/phoenix', verbose=False)
+pb.compute_phoenix_intensities(path='tables/phoenix', verbose=False)
+pb.compute_phoenix_ldcoeffs()
+pb.compute_phoenix_ldints()
+print(pb.content)
+
+
+# There is one extra step that we need to do for phoenix atmospheres: because there are gaps in the coverage of atmospheric parameters, we need to impute those values in order to allow for seamless interpolation. This is achieved by the call to `impute_atmosphere_grid()`. It is a computationally intensive step that can take 10+ minutes.
+
+# In[17]:
+
+
+pb.impute_atmosphere_grid(pb._phoenix_energy_grid)
+pb.impute_atmosphere_grid(pb._phoenix_photon_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ld_energy_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ld_photon_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ldint_energy_grid)
+pb.impute_atmosphere_grid(pb._phoenix_ldint_photon_grid)
+for i in range(len(pb._phoenix_intensity_axes[3])):
+    pb.impute_atmosphere_grid(pb._phoenix_Imu_energy_grid[:,:,:,i,:])
+    pb.impute_atmosphere_grid(pb._phoenix_Imu_photon_grid[:,:,:,i,:])
+
+
+# Now we can compare all three model atmospheres:
+
+# In[18]:
+
+
+plt.xlabel('Temperature [K]')
+plt.ylabel('Inorm [W/m^3]')
+plt.plot(teffs, pb.Inorm(teffs,               atm='blackbody', ldatm='ck2004',  ld_func='linear', ld_coeffs=[0.0]), label='blackbody')
+plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='ck2004',    ldatm='ck2004',  ld_func='linear', ld_coeffs=[0.0]), label='ck2004')
+plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='phoenix',   ldatm='phoenix', ld_func='linear', ld_coeffs=[0.0]), label='phoenix')
+plt.legend(loc='lower right')
+plt.show()
+
+
+# We see that, as temperature increases, model atmosphere intensities can differ quite a bit. That explains why the choice of a model atmosphere is quite important and should be given proper consideration.
+
+# ## Importing Wilson-Devinney response
 # 
 # PHOEBE no longer shares any codebase with the WD code, but for comparison purposes it is sometimes useful to use the same atmosphere tables. If the passband you are registering with PHOEBE has been defined in WD's atmcof.dat and atmcofplanck.dat files, PHOEBE can import those coefficients and use them to compute intensities.
 # 
 # To import a set of WD atmospheric coefficients, you need to know the corresponding index of the passband (you can look it up in the WD user manual available at ftp://ftp.astro.ufl.edu/pub/wilson/lcdc2003/ebdoc2003.2feb2004.pdf.gz) and you need to grab the files ftp://ftp.astro.ufl.edu/pub/wilson/lcdc2003/atmcofplanck.dat.gz and ftp://ftp.astro.ufl.edu/pub/wilson/lcdc2003/atmcof.dat.gz from Bob Wilson's webpage. For this particular passband the index is 22. To import, issue:
 
-# In[16]:
+# In[19]:
 
 
 pb.import_wd_atmcof('atmcofplanck.dat', 'atmcof.dat', 22)
@@ -271,35 +350,37 @@ pb.import_wd_atmcof('atmcofplanck.dat', 'atmcof.dat', 22)
 
 # We can consult the `content` attribute to see the entire set of supported tables, and plot different atmosphere models for comparison purposes:
 
-# In[17]:
+# In[20]:
 
 
 pb.content
 
 
-# In[18]:
+# In[21]:
 
 
 plt.xlabel('Temperature [K]')
-plt.ylabel('Inorm [W/m^2/A]')
-plt.plot(teffs, pb.Inorm(teffs,               atm='blackbody',   ld_func='linear', ld_coeffs=[0.0]), label='blackbody')
-plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='ck2004',      ld_func='linear', ld_coeffs=[0.0]), label='ck2004')
-plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='extern_atmx', ld_func='linear', ld_coeffs=[0.0]), label='wd_atmx')
+plt.ylabel('Inorm [W/m^3]')
+plt.plot(teffs, pb.Inorm(teffs,               atm='blackbody',   ldatm='ck2004',  ld_func='linear', ld_coeffs=[0.0]), label='blackbody')
+plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='ck2004',      ldatm='ck2004',  ld_func='linear', ld_coeffs=[0.0]), label='ck2004')
+plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='phoenix',     ldatm='phoenix', ld_func='linear', ld_coeffs=[0.0]), label='phoenix')
+plt.plot(teffs, pb.Inorm(teffs, loggs, abuns, atm='extern_atmx', ldatm='phoenix', ld_func='linear', ld_coeffs=[0.0]), label='wd_atmx')
 plt.legend(loc='lower right')
 plt.show()
 
 
-# Still an appreciable difference. This hopefully illustrates why excrutiating caution should be exercised at all times when dealing with modeling radiation.
+# Still an appreciable difference. 
 # 
-# Saving the passband table
-# -----------------------------
+# ## Saving the passband table
 # 
 # The final step of all this (computer's) hard work is to save the passband file so that these steps do not need to be ever repeated. From now on you will be able to load the passband file explicitly and PHOEBE will have full access to all of its tables. Your new passband will be identified as `'Custom:mypb'`.
+# 
+# To make PHOEBE automatically load the passband, it needs to be added to one of the [passband directories](http://phoebe-project.org/docs/2.4/api/phoebe.atmospheres.passbands.list_passband_directories) that PHOEBE recognizes. If there are no proprietary aspects that hinder the dissemination of the tables, please consider contributing them to PHOEBE so that other users can use them.
 
-# In[19]:
+# In[22]:
 
 
-pb.save('my_passband.pb')
+pb.save('~/.phoebe/atmospheres/tables/passbands/my_passband.fits')
 
 
-# To make PHOEBE automatically load the passband, add it to the `phoebe/atmospheres/tables/passbands` directory. If there are no proprietary aspects that hinder the dissemination of the tables, please consider contributing them to PHOEBE so that other users can use them.
+# 
