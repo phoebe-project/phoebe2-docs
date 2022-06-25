86764ad1f3b4836e6e55250c96696021d5143cf8
Kyle <kyleconroy@gmail.com>
2022-06-25 11:36:31 -0400

    remove built release-2.5 directory
    * release-* branches will build to development directory to avoid needing manual cleaning, etc

--- release-2.5/tutorials/detach.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Advanced: Detaching from Run Compute
-# ============================
-# 
-# Although experimental support for `detach` still exists, in PHOEBE 2.4+, it is preferred to use [b.export_compute](../api/phoebe.frontend.bundle.Bundle.export_compute.md) or [b.as_client](../api/phoebe.frontend.bundle.Bundle.as_client.md) to manage running scripts outside the main thread.
